0f107ff76a2acf390fd2e2d98023ce172b7f089c
alen <alen.l@captain.in>
2024-02-19 13:56:16 +0530

    test push

--- tiara/src/Components/Navbar/Navbar.jsx
+++ tiara/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart from "../Assets/cart.png";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="logo" />
+        <p>Tiara Boutique</p>
+      </div>
+      <div className="navbar-menu">
+        <li>Home</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </div>
+      <div className="navbar-login">
+        <button>Login</button>
+        <img src={cart} />
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
