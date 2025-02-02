abd1568bc4fb58f308c14d1a7cbf352cecf6c44c
Alen J Ninan <alen.j@captainfresh.in>
2025-02-02 21:29:21 +0530

    added navbar

--- src/Components/componentIndex.js
+++ src/Components/componentIndex.js
@@ -1,3 +1,3 @@
 export { default as Navbar } from "./Navbar/Navbar";
-export { default as Loading } from "./Loading/Loading";
 export { default as Footer } from "./Footer/Footer";
+export { default as Showcase } from "./Showcase/Showcase";
